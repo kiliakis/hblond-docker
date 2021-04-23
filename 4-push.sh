9658702673c96b479040cf0e5c6957a51efbddf8
Konstantinos Iliakis <kiliakis@microlab.ntua.gr>
2021-04-23 16:58:19 +0200

    push and tag

--- 4-push.sh
+++ 4-push.sh
@@ -1,3 +1,4 @@
 #/bin/bash
+docker tag kiliakis/hblond:1.1 172.9.0.240:5000/hblond:1.1
 cat ~/EVOLVE/requirements_setup/password_stdin.txt | docker login 172.9.0.240:5000 --username evolve --password-stdin
 docker push 172.9.0.240:5000/hblond:1.1
